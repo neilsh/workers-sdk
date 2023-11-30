c3bd3da8e4e8555604db9b4a2294b4b1f287836f
bcoll <bcoll@cloudflare.com>
2023-11-30 21:40:33 +0000

    Use custom test runner and reset fetch mock at the start of each file

--- packages/vitest-pool-workers/src/worker/types-globals.d.ts
+++ packages/vitest-pool-workers/src/worker/types-globals.d.ts
@@ -1,8 +1,12 @@
+/* eslint-disable no-var */
+// noinspection ES6ConvertVarToLetConst
+
 declare global {
 	// https://github.com/vitest-dev/vitest/blob/v1.0.0-beta.5/packages/vitest/src/runtime/worker.ts#L49-L69
 	const __vitest_worker__: {
 		config?: { poolOptions?: { miniflare?: SerializedOptions } };
 	};
+	var __console: typeof console;
 }
 
 export {};
